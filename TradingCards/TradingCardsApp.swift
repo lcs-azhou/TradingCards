27b5bae25179453ee5797ea8d4c4ba48caf4841b
Ansheng Zhou <azhou@lcs.on.ca>
2024-01-19 08:39:01 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Ansheng Zhou on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
